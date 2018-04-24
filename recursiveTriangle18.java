95a619b4e2999b96461a5ab3684a92af8f06302d
markallen123 <31445566+markallen123@users.noreply.github.com>
2018-04-24 10:36:24 -0500

    4/24/2018

--- recursiveTriangle18.java
+++ recursiveTriangle18.java
@@ -8,9 +8,9 @@ public class recursiveTriangle18 extends JApplet
 
 /*
    //x is accross and y is down
-   point 1 - Right	A x[0],y[0] (720,600)
-   point 2 - Left	B x[1],y[1]
-   point 3 - Top	C x[2],y[2]
+   point 1 - Right  A x[0],y[0] (720,600)
+   point 2 - Left   B x[1],y[1]
+   point 3 - Top    C x[2],y[2]
    point 4 draws back to point 1 to complete triangle
 
 */  private int[] xPos = {720, 80, 400, 720};
@@ -32,40 +32,59 @@ public class recursiveTriangle18 extends JApplet
    public void paint (Graphics page)
    {
 
-		page.setColor (Color.red);
-		page.drawPolyline (xPos, yPos, xPos.length);
+        page.setColor (Color.red);
+        page.drawPolyline (xPos, yPos, xPos.length);
 
-		Triangle(xPos,yPos,page);
+        Triangle(xPos,yPos,page);
 
    }//end of paint
 
    public void Triangle(int[] xPos, int[] yPos, Graphics page)
    {
-	   //Find the distance between 2 points ex. - x,y & x1,y1
-	   
+       //Find the distance between 2 points ex. - x,y & x1,y1
+       double a = Math.pow((xPos[1]+(xPos[0]-xPos[1])/2)-(xPos[2]+(xPos[1]-xPos[2])/2),2);
+       double b = Math.pow((yPos[1]+(yPos[0]-yPos[1])/2)-(yPos[2]+(yPos[1]-yPos[2])/2),2);
 
-		//if the segment/distance is 300 or so, good length to stop
-		//{
-			//find the mid points of each line segment
+        //if the segment/distance is 300 or so, good length to   stop
+        if(Math.sqrt(a+b) <= 20)
+        {
+            return;
+        }
+        else
+        {
+            int[] xMid = new int[3];
+            int[] yMid = new int[3];
+            xMid[0] = (xPos[1]+(xPos[0]-xPos[1]/2));
+            xMid[1] = (xPos[2]+(xPos[1]-xPos[2]/2));
+            xMid[2] = (xPos[3]+(xPos[2]-xPos[3]/2));
+            
+            yMid[0] = (yPos[1]+(yPos[0]-yPos[1]/2));
+            yMid[1] = (xPos[2]+(yPos[1]-yPos[2]/2));
+            yMid[2] = (xPos[3]+(yPos[2]-yPos[3]/2));
+            
+            
+        }
+        //{
+            //find the mid points of each line segment
 
 
-			//make the x and y array (3 points + first point to finish triangle)
+            //make the x and y array (3 points + first point to finish triangle)
 
 
-			//draw the Triangle
-			//page.drawPolyline (xNew, yNew, xNew.length);
+            //draw the Triangle
+            //page.drawPolyline (xNew, yNew, xNew.length);
 
-			//create x,y Array using the midpoints you calculated
-			//example
-
-
-
-
-			// Recursive calls for each section of triangle
+            //create x,y Array using the midpoints you calculated
+            //example
 
 
 
-		//}
+
+            // Recursive calls for each section of triangle
+
+
+
+        //}
 
 
 
